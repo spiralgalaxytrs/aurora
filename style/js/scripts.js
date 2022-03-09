112421441f8507009cf509a58719b94fb76cc13b
spiralgalaxytrs <80592602+spiralgalaxytrs@users.noreply.github.com>
2022-03-09 17:15:13 +0530

    go to top

--- style/js/scripts.js
+++ style/js/scripts.js
@@ -91,9 +91,27 @@ $(document).ready(function() {
 	}
 	enableFlickitySlider();
 	enableFlickityCarousel();
-    /*-----------------------------------------------------------------------------------*/
-    /*	CUBE PORTFOLIO
-    /*-----------------------------------------------------------------------------------*/
+    
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+document.body.scrollTop = 0; // For Safari
+document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+    
     var $cubeinline = $('#cube-inline');
 	$cubeinline.cubeportfolio({
         filters: '#cube-inline-filter',
